dcea51215fa3760a003e88f0b3c72f14836e526e
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-03-07 23:53:58 -0300

    added functions to settings in the task

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,4 +1,4 @@
-import './styles.css'
+import './styles.css';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AddIcon from '@mui/icons-material/Add';
 import { useReducer, useState } from 'react';
@@ -8,6 +8,7 @@ import { Task } from '../Task';
 export const Tasks = () => {
     const [taskInput, setTaskInput] = useState('');
     const [tasks, dispatch] = useReducer(listReducer, []);
+    const [showModal, setShowModal] = useState(false);
 
     const handleToggleDone = (id: number) => {
         dispatch({
@@ -47,11 +48,44 @@ export const Tasks = () => {
         });
     };
 
+    const handleClearAll = () => {
+        dispatch({
+            type: 'clearAll',
+        });
+        setShowModal(false);
+    };
+
+    const handleClearDone = () => {
+        dispatch({
+            type: 'clearDone',
+        });
+        setShowModal(false);
+    }
+
+    const handleDoneAll = () => {
+        dispatch({
+            type: 'doneAll',
+        });
+        setShowModal(false);
+    };
+
     return (
         <div className='tasks'>
             <div className="taskHeader">
                 <h1>Tasks</h1>
-                <MoreVertIcon className='more' />
+                <div className="taskHeader--btn" onClick={() => setShowModal(true)}>
+                    <MoreVertIcon className='more' />
+                </div>
+                {showModal && (
+                    <div className='settings2'>
+                        <div className='settings-content2'>
+                            <button onClick={handleClearAll}>Excluir todas as tasks</button>
+                            <button onClick={handleClearDone}>Excluir as tasks feitas</button>
+                            <button onClick={handleDoneAll}>Marcar todas como feita</button>
+                            <button onClick={() => setShowModal(false)}>Fechar</button>
+                        </div>
+                    </div>
+                )}
             </div>
             <div className="taskArea">
                 <input type="text" placeholder='Nome da tarefa' value={taskInput} onChange={(e) => setTaskInput(e.target.value)} />
--- src/reducers/listReducer.ts
+++ src/reducers/listReducer.ts
@@ -5,57 +5,101 @@ type AddAction = {
     payload: {
         text: string;
     };
-}
+};
+
 type EditTextAction = {
     type: 'editText';
     payload: {
         id: number;
         newText: string;
-    }
-}
+    };
+};
+
 type ToggleDoneAction = {
     type: 'toggleDone';
     payload: {
         id: number;
-    }
-}
+    };
+};
+
 type RemoveAction = {
     type: 'remove';
     payload: {
         id: number;
-    }
-}
+    };
+};
 
-type ListActions = AddAction | EditTextAction | ToggleDoneAction | RemoveAction;
+type ClearAllAction = {
+    type: 'clearAll';
+};
+
+type ClearDoneAction = {
+    type: 'clearDone';
+};
+
+type DoneAllAction = {
+    type: 'doneAll';
+};
+
+type ListActions =
+    | AddAction
+    | EditTextAction
+    | ToggleDoneAction
+    | RemoveAction
+    | ClearAllAction
+    | DoneAllAction
+    | ClearDoneAction;
 
 export const listReducer = (list: Item[], action: ListActions) => {
     switch (action.type) {
         case 'add':
-            return [...list, {
-                id: list.length,
-                text: action.payload.text,
-                done: false
-            }]
+            return [
+                ...list,
+                {
+                    id: list.length,
+                    text: action.payload.text,
+                    done: false
+                }
+            ];
+
         case 'editText':
             return list.map(t => {
                 if (t.id === action.payload.id) {
-                    t.text = action.payload.newText;
+                    return {
+                        ...t,
+                        text: action.payload.newText
+                    };
                 }
                 return t;
             });
+
         case 'toggleDone':
             return list.map((t) => {
                 if (t.id === action.payload.id) {
-                  return {
-                    ...t,
-                    done: !t.done,
-                  };
+                    return {
+                        ...t,
+                        done: !t.done,
+                    };
                 }
                 return t;
-              });
+            });
+
         case 'remove':
             return list.filter(t => t.id !== action.payload.id);
+
+        case 'clearAll':
+            return [];
+
+        case 'doneAll':
+            return list.map(task => ({
+                ...task,
+                done: true,
+            }));
+
+        case 'clearDone':
+            return list.filter(task => task.done === false);
+
         default:
             return list;
     }
-}
+};
